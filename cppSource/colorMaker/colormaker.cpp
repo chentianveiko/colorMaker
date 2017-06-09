2e8430fc7b962f1a5056471bb8d12f2d23176ef9
Veiko <303643087@qq.com>
2017-06-09 11:33:05 +0800

    C++类部份基础工作完成,编译能够完全通过,准备QML部份

--- cppSource/colorMaker/colormaker.cpp
+++ cppSource/colorMaker/colormaker.cpp
@@ -1,8 +1,13 @@
 #include "colormaker.h"
+#include <QDateTime>
+#include <QTimerEvent>
 
 ColorMaker::ColorMaker(QObject *parent) : QObject(parent)
+  ,myColor(Qt::black)
+  ,myAlgorithm(randomRGB)
+  ,mColorTimer(0)
 {
-
+    qsrand(QDateTime::currentDateTime().toTime_t());
 }
 
 ColorMaker::~ColorMaker()
@@ -10,23 +15,83 @@ ColorMaker::~ColorMaker()
 
 }
 
-GenerateAlgorithm ColorMaker::algorithm() const
+ColorMaker::generate ColorMaker::algorithm() const
 {
-
+    return myAlgorithm;
 }
 
-void ColorMaker::setAlgorithm(GenerateAlgorithm algorithm)
+void ColorMaker::setAlgorithm(ColorMaker::generate algorithm)
 {
+    myAlgorithm = algorithm;
+}
 
+QColor ColorMaker::color() const
+{
+    return myColor;
+}
+
+void ColorMaker::setColor(QColor m_color)
+{
+    myColor = m_color;
+}
+
+QColor ColorMaker::colorTime() const
+{
+    QTime time = QTime::currentTime();
+    int r = time.hour();
+    int g = time.minute()*2;
+    int b = time.second()*4;
+
+    return QColor::fromRgb(r, g, b);
+}
+
+void ColorMaker::timerEvent(QTimerEvent *e)
+{
+    if(e->timerId() == mColorTimer){
+        switch (myAlgorithm) {
+        case randomRGB:
+            myColor.setRgb(qrand()%255, qrand()%255, qrand()%255);
+            break;
+
+        case randomRed:
+            myColor.setRed(qrand() % 255);
+            break;
+
+        case randomGreen:
+            myColor.setGreen(qrand() % 255);
+            break;
+
+        case randomBlue:
+            myColor.setBlue(qrand() % 255);
+            break;
+
+        case increase:
+            int r = myColor.red() + 10;
+            int g = myColor.green() + 10;
+            int b = myColor.blue() + 10;
+            myColor.setRgb(r%255,g%255,b%255);
+            break;
+        }
+
+        emit colorChanged(myColor);
+        emit currentTime(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
+    }else{
+        QObject::timerEvent(e);
+    }
 }
 
 void ColorMaker::start()
 {
-
+    if(mColorTimer == 0){
+        mColorTimer = startTimer(1000);
+    }
 }
 
 void ColorMaker::stop()
 {
-
+    if(mColorTimer > 0){
+        killTimer(mColorTimer);
+        mColorTimer = 0;
+    }
 }
 
--- cppSource/colorMaker/colormaker.h
+++ cppSource/colorMaker/colormaker.h
@@ -2,16 +2,49 @@
 #define COLORMAKER_H
 
 #include <QObject>
+#include <QTimer>
+#include <QColor>
 
 class ColorMaker : public QObject
 {
     Q_OBJECT
+
+    // 声明可以被元对象访问的枚举类型: Q_ENUMS(枚举类型的名字)
+    Q_ENUMS(generate)
+
+    // 声明可以被元对象访问,修改的属性,并且在该属性发生变化时发射colorChanged信号
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+    Q_PROPERTY(QColor colorTime READ colorTime)
+
+public:
+    //枚举参数
+    enum generate{
+        randomRGB,
+        randomRed,
+        randomGreen,
+        randomBlue,
+        increase,
+    };
+
+private:
+    QColor myColor;
+    generate myAlgorithm;
+    int mColorTimer;
+
+
 public:
     explicit ColorMaker(QObject *parent = 0);
     ~ColorMaker();
 
-    Q_INVOKABLE GenerateAlgorithm algorithm() const;
-    Q_INVOKABLE void setAlgorithm(GenerateAlgorithm algorithm);
+    // Q_INVOKABLE放在类成员函数的返回类型前作为修饰,该函数就能被元对象调用了
+    Q_INVOKABLE generate algorithm() const;
+    Q_INVOKABLE void setAlgorithm(generate algorithm);
+    Q_INVOKABLE QColor color() const;
+    Q_INVOKABLE void setColor(QColor m_color);
+    Q_INVOKABLE QColor colorTime() const;
+
+protected:
+    void timerEvent(QTimerEvent *e);
 
 signals:
     void colorChanged(const QColor & color);
